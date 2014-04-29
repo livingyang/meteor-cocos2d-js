6ee0ebab69ebbbbd1775b1ee82bd7e8beb30d0f8
livingyang <lhm.cold@163.com>
2014-04-29 11:14:15 +0800

    Add: gulp release
    Add: busters to project.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var bust = require('gulp-buster');
+var jeditor = require("gulp-json-editor");
 
 var paths = {
 	scripts: ['src/main.begin.coffee', 'src/main/**/*.coffee', 'src/main.end.coffee'],
 	app: 'meteor/public/app/',
-	public: 'meteor/public/'
+	public: 'meteor/public/',
+	cocos2d: 'cocos2d-js/app/'
 };
 
 gulp.task('doctor', function() {
@@ -34,9 +36,17 @@ gulp.task('uglify', ['build'], function() {
 	.pipe(gulp.dest(paths.app));
 });
 
-gulp.task('release', function () {
-	return gulp.src(paths.app + "**")
-	.pipe(gulp.dest('cocos2d-js/app/'));
+gulp.task('release', ['uglify', 'busters'], function () {
+	// 1 copy resource
+	gulp.src(paths.app + "**")
+	.pipe(gulp.dest(paths.cocos2d));
+
+	// 2 add busters to project.json
+	gulp.src(paths.cocos2d + 'project.json')
+	.pipe(jeditor({
+		busters: JSON.parse(String(fs.readFileSync(paths.public + 'busters.json')))
+	}))
+	.pipe(gulp.dest(paths.cocos2d));
 })
 
 gulp.task('busters', function() {
@@ -48,4 +58,4 @@ gulp.task('busters', function() {
 	process.chdir(oldCwd);
 });
 
-gulp.task('default', ['build', 'busters', 'release']);
+gulp.task('default', ['build', 'busters']);
