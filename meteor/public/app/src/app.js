8de6aead63d0a82b365297d715e9556e6af48d3d
livingyang <lhm.cold@163.com>
2014-04-30 20:55:53 +0800

    Update: move cc.game.onStart to front

--- meteor/public/app/src/app.js
+++ meteor/public/app/src/app.js
@@ -1,13 +1,6 @@
 (function() {
   var TestScene;
 
-  TestScene = cc.Scene.extend({
-    onEnter: function() {
-      this._super();
-      return this.addChild(cc.LayerColor.create(cc.color(255, 255, 0, 255)));
-    }
-  });
-
   cc.game.onStart = function() {
     cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
@@ -15,5 +8,12 @@
       return cc.director.runScene(new TestScene());
     });
   };
+
+  TestScene = cc.Scene.extend({
+    onEnter: function() {
+      this._super();
+      return this.addChild(cc.LayerColor.create(cc.color(255, 255, 0, 255)));
+    }
+  });
 
 }).call(this);
--- src/app/app.coffee
+++ src/app/app.coffee
@@ -1,12 +1,11 @@
-TestScene = cc.Scene.extend
-	onEnter: ->
-		@_super()
-		@addChild cc.LayerColor.create cc.color 255, 255, 0, 255
-
 cc.game.onStart = ->
 	cc.view.setDesignResolutionSize 800, 450, cc.ResolutionPolicy.SHOW_ALL
 	cc.view.resizeWithBrowserSize true
 
 	cc.LoaderScene.preload {}, ->
 		cc.director.runScene new TestScene()
-		
+				
+TestScene = cc.Scene.extend
+	onEnter: ->
+		@_super()
+		@addChild cc.LayerColor.create cc.color 255, 255, 0, 255
